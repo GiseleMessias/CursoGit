36649530df1cab1277b0e77d115f7f16fc904065
Gisele de Castro Faustino <giselecf@primeit.com.br>
2021-05-24 11:30:09 -0300

    meu segundo commit

--- TesteGi.java
+++ TesteGi.java
@@ -1,5 +1,9 @@
 public class TesteGi{
     public static void main (String[] args){
          System.out.println("Olá Mundo!!!");
+
+         ASerChamada saida = new ASerChamada();
+
+         saida.testePrint();
     }
 }
